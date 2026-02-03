6e0fcfb5def503a4262148232881cbfcac8f9a8c
Ilham Rahmat Akbar <ilhamrhmtkbr@gmail.com>
2026-02-03 10:11:23 +0700

    fix: frontend-user svg ga tampil

--- frontend-user/src/components/SvgComp.jsx
+++ frontend-user/src/components/SvgComp.jsx
@@ -3,7 +3,7 @@ import {memo} from 'react'
 const SvgComp = memo((props) => {
     return (
         <svg className={props.rule}>
-            <use xlinkHref={import.meta.env.BASE_URL + `/${props.file}.svg#${props.icon}`}></use>
+            <use xlinkHref={import.meta.env.BASE_URL + `${props.file}.svg#${props.icon}`}></use>
         </svg>
     )
 });
